11ea14e478e50d7cff801d789f43f59a43e3120d
Grimm1998 <Andrey.Grimm.1998@gmail.com>
2017-01-13 23:34:48 +0200

    Start project 6

---
+++ Проект6/Header.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <iostream>
+#include <string>
+
+class employee
+{
+private:
+	std::string name;
+	long num;
+public:
+
+};
+
+#endif // !_HEADER_H_
---
+++ Проект6/Source.cpp
@@ -0,0 +1,11 @@
+#include "Header.h"
+
+int main(int argc, char **argv)
+{
+	system("color f0");
+
+	
+
+	system("pause");
+	return NULL;
+}
